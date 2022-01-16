d3a3a9d9e9115b71aec93d3bb34349500532fb09
Luis Zaman <luis.zaman@gmail.com>
2022-01-16 16:40:27 -0500

    adding mutation stub

--- sketch.js
+++ sketch.js
@@ -2,22 +2,44 @@ var gui;
 var resetButton;
 var landscapeLayer;
 var org_test;
+var world_x = 800;
+var world_y = 600;
+
 
 function Organism() {
-    this.x = 100;
-    this.y = 100;
+    this.x = 10;
+    this.y = 10;
 }
 
 //"Sketching" a fitness function for starters
 Organism.prototype.getFitness = function () {
-
   //might have to combine future layers...?
   let spot_color = landscapeLayer.get(this.x, this.y);
   return spot_color[3];
 }
 
+Organism.prototype.mutate = function() {
+  let mutate_x = random() < 0.5? 0 : 1;
+  let mutate_positive = random() < 0.5? 0 : 1;
+  //integer mutation sizes
+  let mutation_offset = mutate_positive? 1:-1;
+
+  if (mutate_x) {
+    this.x += mutation_offset;
+  } else {
+    this.y += mutation_offset;
+  }
+
+  //torus topology
+  this.x = this.x < 0 ? world_x + this.x : this.x;
+  this.y = this.y < 0 ? world_y + this.y : this. y;
+  this.x = this.x % world_x;
+  this.y = this.y % world_y;
+
+}
 Organism.prototype.draw = function() {
   fill('orange');
+  noStroke();
   ellipse(this.x, this.y, 5, 5);
 }
 
@@ -25,7 +47,7 @@ function setup() {
   createCanvas(800, 600);
   org_test = new Organism();
 
-  landscapeLayer = createGraphics(800,600);
+  landscapeLayer = createGraphics(world_x,world_y);
   gui = createGui();
   resetButton = createButton("Reset", 30, 30);
   gui.loadStyle("Blue");
@@ -46,6 +68,7 @@ function draw() {
   clear();
   image(landscapeLayer, 0, 0)
   org_test.draw();
+  org_test.mutate();
   drawGui();
 }
 
