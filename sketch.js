06fdb5973bf8965a1fe454ae518a424a44eb3c2d
Luis Zaman <luis.zaman@gmail.com>
2022-01-15 23:28:56 -0500

    these intermediate commits are for documentation of the process, in case Academic Innovation would like to have the data

--- sketch.js
+++ sketch.js
@@ -1,28 +1,55 @@
 var gui;
-var b;
+var resetButton;
+var landscapeLayer;
+var org_test;
+
+function Organism() {
+    this.x = 100;
+    this.y = 100;
+}
+
+//"Sketching" a fitness function for starters
+Organism.prototype.getFitness = function () {
+
+  //might have to combine future layers...?
+  let spot_color = landscapeLayer.get(this.x, this.y);
+  return spot_color[3];
+}
+
+Organism.prototype.draw = function() {
+  fill('orange');
+  ellipse(this.x, this.y, 5, 5);
+}
 
 function setup() {
-    createCanvas(800, 600);
-    gui = createGui();
-    b = createButton("Button", 30, 30);
-    gui.loadStyle("Blue");
+  createCanvas(800, 600);
+  org_test = new Organism();
 
+  landscapeLayer = createGraphics(800,600);
+  gui = createGui();
+  resetButton = createButton("Reset", 30, 30);
+  gui.loadStyle("Blue");
+}
 
+function draw() {
+  if (mouseIsPressed) {
+    landscapeLayer.fill(0, 0, 0, 1);
+    landscapeLayer.noStroke();
+    landscapeLayer.ellipse(mouseX, mouseY, 20, 20);
+  }
+  if (resetButton.isPressed) {
+    console.log(org_test);
+    console.log(org_test.getFitness());
+    resetWorld();
   }
   
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0, 0, 0, 1);
-    } else {
-      fill(0, 0, 0, 0);
-    }
-    noStroke();
-    ellipse(mouseX, mouseY, 30, 30);
-    drawGui();
+  clear();
+  image(landscapeLayer, 0, 0)
+  org_test.draw();
+  drawGui();
+}
 
-  }
-
-  function mousePressed() {
-    loadPixels();
-    console.log(pixels)
-  }
+//This will eventually get more complex, so pulling it out now
+function resetWorld() {
+  landscapeLayer.clear();
+}
